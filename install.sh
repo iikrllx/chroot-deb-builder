068e219faf60bdcffbc20aefe73a448aa7781ac1
Kirill Rekhov <krekhov.dev@gmail.com>
2024-02-11 16:10:18 +0300

    rename main script / update
    $ git mv debootstrap.sh deb-builder.sh
    This name fits better

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 
 set -ex
 
-for f in builder.sh chroot-env.sh debootstrap.sh; do
+for f in builder.sh chroot-env.sh deb-builder.sh; do
 	sudo install $f /usr/local/bin
 done
 
 sudo mkdir -p /usr/share/sounds/for-script
 sudo install sound/* /usr/share/sounds/for-script
-
-echo $?
